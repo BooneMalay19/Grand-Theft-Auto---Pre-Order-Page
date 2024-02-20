b0c46fc7007ed5bc76e6aa251a374e80076b6071
Pedro Brito de Souza <144711331+BooneMalay19@users.noreply.github.com>
2024-02-20 19:44:01 -0300

    Add files via upload

---
+++ src/js/scriptopcao.js
@@ -0,0 +1,6 @@
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
